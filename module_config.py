5878d47ccc46b5a4fcfc009981e8cd9bf3e6aebe
ilyava1 <98491946+ilyava1@users.noreply.github.com>
2022-08-24 11:14:36 +0300

    Update module_config.py
    deleted personal token data

--- module_config.py
+++ module_config.py
@@ -5,7 +5,4 @@ params = [
 {'param_name': 'dir_for_vk_photos', 'param_description': 'Адрес директории на локальном диске для сохранения фотографий', 'param_body': 'C:/Unic_VK_photos/'},
 {'param_name': 'yadisk_filepath', 'param_description': 'Адрес директории на Яндекс-диске для загрузки фотографий', 'param_body': 'Unic_VK_photos/'},
 {'param_name': 'photo_quantity', 'param_description': 'Макс кол-во фото для сохранения на Я.Диск', 'param_body': 5},
-{'param_name': 'vk_token', 'param_description': 'Илья Адамов', 'param_body': 'vk1.a.f9qcaq1RKrp6hOoilwnZw_ZiURmpopJAOAOyJNHusSPiI-033pqJvp1DXU0nQsN4r1JaVgSocHHD4pdc6HdlIK1eaH7VTiWrl6p2Ijxa92C43vH97AFnhiuBHWzkjwNLdHvF4uQcf0hsSez7EIWVT7_py15kRjON6o5Cf5P4Rl4NGSDVqsv8JqDPTLgcOFiI'},
-{'param_name': 'vk_user_id', 'param_description': 'Идентификатор пользователя ВКонтакте', 'param_body': '375525321'},
-{'param_name': 'ya_token', 'param_description': 'Ilyava1', 'param_body': 'AQAAAAAKkdsQAADLW2poyEmiCEXerSytE-Oeq6g'},
 ]
